c2b0f456e8c46668ee4398f8abb78c5763ab4c38
Oliver Stigley <oliverstigley@gmail.com>
2016-06-13 16:21:31 +1200

    start vote test passing

--- voting-server/src/core.js
+++ voting-server/src/core.js
@@ -1,3 +1,12 @@
-import {List} from 'immutable'
+import {List, Map} from 'immutable'
+
 
 export const setEntries = (state, entries) => state.set('entries', List(entries))
+
+export const next = (state) => {
+	const entries = state.get('entries')
+	return state.merge({
+		vote: Map({pair: entries.take(2)}),
+		entries: entries.skip(2)
+	})
+}
