fa58e63c0bc5af1f628c63b467217c1aff72f76a
Oliver Stigley <oliverstigley@gmail.com>
2016-06-13 16:43:43 +1200

    voting test passing

--- voting-server/src/core.js
+++ voting-server/src/core.js
@@ -6,7 +6,9 @@ export const setEntries = (state, entries) => state.set('entries', List(entries)
 export const next = (state) => {
 	const entries = state.get('entries')
 	return state.merge({
-		vote: Map({pair: entries.take(2)}),
-		entries: entries.skip(2)
+		vote: Map({pair: entries.take(2)}), //takes first x amount from iterable. 
+		entries: entries.skip(2)  // same as .pop().pop() // entries except first 2 elements
 	})
 }
+
+export const vote = (state, entry) => state.updateIn(['vote', 'tally', entry], 0, tally => tally+1)
