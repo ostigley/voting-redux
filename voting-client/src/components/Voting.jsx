cb5bbb0510e11305396ab6963f473aa174a66a53
Oliver Stigley <oliverstigley@gmail.com>
2016-06-14 17:39:56 +1200

    vote, winner and voting componenets split out

---
+++ voting-client/src/components/Voting.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Winner from './Winner'
+import Vote from './Vote'
+
+export default class Voting extends React.Component {
+	
+	render () {
+		return <div className="voting">
+      {this.props.winner ? 
+      	<Winner winner={this.props.winner} />:
+      	<Vote {...this.props}/>
+      }
+    </div>
+	}
+}
